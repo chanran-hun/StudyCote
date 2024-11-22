e3610f91296c70f8fce2a9c31da310bb3b6226e3
JEONG HUN LEE <jhunlee56@naver.com>
2024-11-22 13:46:12 +0900

    N 과 M (9)

--- 15652/15652.cpp
+++ 15652/15652.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
 int N, M;
-vector<int> result;
-void N_M(int depth) {
-	if (depth == M) {
+vector<int> result;		//결과값 저장 수열.
+void N_M(int depth) {	//깊이를 입력받아서 종료조건을 만듭니다.
+	if (depth == M) {	//깊이가 M과 같을 때 출력
 		for (int n : result) {
 			cout << n << " ";
 		}
@@ -12,13 +13,13 @@ void N_M(int depth) {
 		return;
 	}
 
-	for (int i = 1; i <= N; i++) {
-		if (!result.empty() && i < result.back()) {
+	for (int i = 1; i <= N; i++) {					//1부터 시작하여 N까지 반복
+		if (!result.empty() && i < result.back()) {	//이전 수보다 크거나 같아야 함.
 			continue;
 		}
-		result.push_back(i);
-		N_M(depth + 1);
-		result.pop_back();
+		result.push_back(i);						//조건에 만족한다면 결과 수열에 수 추가
+		N_M(depth + 1);								//depth 증가시키기
+		result.pop_back();							//출력 한 후에 가장 뒤에 있는 수 삭제
 	}
 }
 
---
+++ 15663/15663.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+vector<int> arr;
+vector<int> result;
+vector<bool> visited;
+
+void N_M(int depth) {
+	if (depth == M) {
+		for (int n : result) {
+			cout << n << " ";
+		}
+		cout << '\n';
+		return;
+	}
+
+	int lastUsed = -1;
+	for (int i = 0; i < N; i++) {
+		if (visited[i] || lastUsed == arr[i]) {
+			continue;
+		}
+		result.push_back(arr[i]);
+		lastUsed = arr[i];
+		visited[i] = true;
+		N_M(depth + 1);
+		result.pop_back();
+		visited[i] = false;
+	}
+}
+
+int main() {
+	cin >> N >> M;
+	arr.resize(N);
+	visited.resize(N, false);
+
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+
+	sort(arr.begin(), arr.end());
+
+	N_M(0);
+
+	return 0;
+}
