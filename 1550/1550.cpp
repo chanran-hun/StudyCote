dd204839edb64192aa11ca514603ec50cf48468f
Jeong Hun <jhunlee56@naver.com>
2024-12-01 18:19:27 +0900

    16진수 코멘트 완료

--- 1550/1550.cpp
+++ 1550/1550.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 using namespace std;
 int HexToDec(string target) {
-	int result = 0;
+	int result = 0;	//반환 할 결과값
 
 	for (int i = 0; i < target.length(); i++) {
-		if ('0' <= target[i] && target[i] <= '9') {
-			int temp = target[i] - '0';
+		if ('0' <= target[i] && target[i] <= '9') {	//자리의 수가 0~9일 경우
+			int temp = target[i] - '0';				//자리의 수를 구합니다.
 			for (int j = 0; j < target.length() - 1 - i; j++) {
-				temp *= 16;
+				temp *= 16;							//그 수가 10진수로 어떤 수에 해당하는지 구합니다.
 			}
-			result += temp;
+			result += temp;							//결과값에 그 값을 합해줍니다.
 		}
 
-		if ('A' <= target[i] && target[i] <= 'F') {
+		if ('A' <= target[i] && target[i] <= 'F') {	//자리의 수가 A~F일 때의 경우로, 위의 과정과 동일합니다.
 			int temp = target[i] - 'A' + 10;
 			for (int j = 0; j < target.length() - 1 - i; j++) {
 				temp *= 16;
@@ -20,7 +20,7 @@ int HexToDec(string target) {
 			result += temp;
 		}
 	}
-	return result;
+	return result;	//결과값을 반환합니다.
 }
 
 int main() {
